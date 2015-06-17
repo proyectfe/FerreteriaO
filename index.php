7ed90a09c0299293199be5c2e40ed90bc15bf399
RPoblete <rod.pobletec@alumnos.duoc.cl>
2015-06-16 20:33:05 -0400

    Login

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-echo("Hola mundo");
-?>
+<p align="center">
+<form name="login" method="post" action="ingreso.php">
+<table style="font-family: verdana; font-size: 12px">
+	<tbody>
+		<tr>
+			<td>Nombre de usuario:</td>
+			<td><input type="text" maxlength="20"></td>
+		</tr>
+		<tr>
+			<td>Contrase&ntilde;a:</td>
+			<td><input type="password" maxlength="20"></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center">
+				<a href="javascript:login.submit()">
+				<img src="images/login.jpg" height="100">
+				</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</form>
+</p>
